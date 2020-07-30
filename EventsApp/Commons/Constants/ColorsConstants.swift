743e80e4f8b45c395e59f2d94e292617b67448dd
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-07-30 06:43:39 +0200

    [FEATURE][COLORS] * Some colors were added

---
+++ EventsApp/Commons/Constants/ColorsConstants.swift
@@ -0,0 +1,14 @@
+//
+// Created by Everis on 29/07/2020.
+// Copyright (c) 2020 Alan Casas. All rights reserved.
+//
+
+import Foundation
+
+struct ColorsConstants {
+    static let blueGreen = "blueGreen"
+    static let darkBlue = "darkBlue"
+    static let gray = "gray"
+    static let lightGray = "lightGray"
+    static let mainColor = "mainColor"
+}
