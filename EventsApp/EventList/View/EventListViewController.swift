044dd1de3755dff9b4d4547287db4d7cae927023
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-07-30 06:44:17 +0200

    [FEATURE][EVENT LIST] * Event List UI was modified

--- EventsApp/EventList/View/EventListViewController.swift
+++ EventsApp/EventList/View/EventListViewController.swift
@@ -12,7 +12,31 @@ class EventListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .cyan
+        setupUI()
     }
 
+    func setupUI () {
+        self.view.backgroundColor = UIColor(named: ColorsConstants.mainColor)
+        setupNavigation()
+    }
+
+    func getRightNavigationBar () -> UIBarButtonItem {
+        let plusImage = UIImage(systemName: "plus.circle.fill")
+        let right = UIBarButtonItem(image: plusImage, style: .plain, target: self, action: #selector(rightButtonPressed))
+        right.tintColor = UIColor(named: ColorsConstants.blueGreen)
+        return right
+    }
+
+    func setupNavigation () {
+        navigationItem.rightBarButtonItem = getRightNavigationBar()
+        navigationItem.title = EventListConstants.eventListTitle
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+}
+
+extension EventListViewController {
+    @objc func rightButtonPressed () {
+
+    }
 }
